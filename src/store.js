21ecdb90fb00cf13c309066eb1ad0b06f486c79d
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-02-19 22:36:45 +0800

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import todos from './containers/App/reducer';
 
 const reducer = combineReducers({
   todos,
 });
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
