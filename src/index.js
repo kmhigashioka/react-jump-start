fa8c99c3f9e69b3c8c2989665afe137aa1499602
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-09-17 23:27:12 +0800

    Reboot App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import 'todomvc-app-css/index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import store from './store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
