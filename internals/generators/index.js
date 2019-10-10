12ec7a676991280086f8a8bc38e07e82e517ba7e
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-10-10 20:27:01 +0800

    Add cli for container

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -1,5 +1,7 @@
 const componentGenerator = require('./component');
+const containerGenerator = require('./container');
 
 module.exports = function generator(plop) {
   plop.setGenerator('component', componentGenerator);
+  plop.setGenerator('container', containerGenerator);
 };
