502740b9bf535c84537fd7d620888984c363d8df
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-11-26 22:56:54 +0800

    Add hooks for context access

--- src/containers/Language/LanguageContext.ts
+++ src/containers/Language/LanguageContext.ts
@@ -1,10 +1,20 @@
 import React from 'react';
 import { TLanguageContext } from './types';
 
-const LanguageContext = React.createContext<TLanguageContext>({
-  setLocale: () => {},
-});
+const LanguageContext = React.createContext<TLanguageContext | undefined>(
+  undefined,
+);
 
 LanguageContext.displayName = 'LanguageContext';
+
+export const useLanguageContext = (): TLanguageContext => {
+  const context = React.useContext(LanguageContext);
+  if (context === undefined) {
+    throw new Error(
+      'useLanguageContext must be used within a LanguageContextProvider.',
+    );
+  }
+  return context;
+};
 
 export default LanguageContext;
