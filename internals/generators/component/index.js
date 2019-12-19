6e19f4e3849d270427d1ff81781af7c730524453
Kazuhito Higashioka <kmhigashioka@gmail.com>
2019-12-19 19:28:09 +0800

    Configurable unit test generator (#46)
    * Add configuration for generating unit test
    
    * Add configuration for generating unit test

--- internals/generators/component/index.js
+++ internals/generators/component/index.js
@@ -19,27 +19,40 @@ module.exports = {
         return 'Component name is required.';
       },
     },
-  ],
-  actions: [
     {
-      type: 'add',
-      path: `${componentDir}/index.ts`,
-      templateFile: './component/index.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: `${componentDir}/{{name}}.tsx`,
-      templateFile: './component/Component.tsx.hbs',
-    },
-    {
-      type: 'add',
-      path: `${componentDir}/types.ts`,
-      templateFile: './component/types.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: `${componentDir}/{{name}}.spec.tsx`,
-      templateFile: './component/Component.spec.tsx.hbs',
+      type: 'confirm',
+      name: 'wantUnitTests',
+      default: true,
+      message: 'Do you want to add unit test(s)?',
     },
   ],
+  actions: data => {
+    const actions = [
+      {
+        type: 'add',
+        path: `${componentDir}/index.ts`,
+        templateFile: './component/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: `${componentDir}/{{name}}.tsx`,
+        templateFile: './component/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: `${componentDir}/types.ts`,
+        templateFile: './component/types.ts.hbs',
+      },
+    ];
+
+    if (data.wantUnitTests) {
+      actions.push({
+        type: 'add',
+        path: `${componentDir}/{{name}}.spec.tsx`,
+        templateFile: './component/Component.spec.tsx.hbs',
+      });
+    }
+
+    return actions;
+  },
 };
--- internals/generators/container/index.js
+++ internals/generators/container/index.js
@@ -19,37 +19,50 @@ module.exports = {
         return 'Container name is required.';
       },
     },
-  ],
-  actions: [
     {
-      type: 'add',
-      path: `${containerDir}/index.ts`,
-      templateFile: './container/index.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: `${containerDir}/{{name}}.tsx`,
-      templateFile: './container/Container.tsx.hbs',
-    },
-    {
-      type: 'add',
-      path: `${containerDir}/types.ts`,
-      templateFile: './container/types.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: `${containerDir}/{{name}}.spec.tsx`,
-      templateFile: './container/Container.spec.tsx.hbs',
-    },
-    {
-      type: 'add',
-      path: `${containerDir}/{{name}}Context.ts`,
-      templateFile: './container/ContainerContext.ts.hbs',
-    },
-    {
-      type: 'add',
-      path: `${containerDir}/Loadable.tsx`,
-      templateFile: './container/Loadable.tsx.hbs',
+      type: 'confirm',
+      name: 'wantUnitTests',
+      default: true,
+      message: 'Do you want to add unit test(s)?',
     },
   ],
+  actions: data => {
+    const actions = [
+      {
+        type: 'add',
+        path: `${containerDir}/index.ts`,
+        templateFile: './container/index.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: `${containerDir}/{{name}}.tsx`,
+        templateFile: './container/Container.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: `${containerDir}/types.ts`,
+        templateFile: './container/types.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: `${containerDir}/{{name}}Context.ts`,
+        templateFile: './container/ContainerContext.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: `${containerDir}/Loadable.tsx`,
+        templateFile: './container/Loadable.tsx.hbs',
+      },
+    ];
+
+    if (data.wantUnitTests) {
+      actions.push({
+        type: 'add',
+        path: `${containerDir}/{{name}}.spec.tsx`,
+        templateFile: './container/Container.spec.tsx.hbs',
+      });
+    }
+
+    return actions;
+  },
 };
